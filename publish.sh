f60a98f4f4315d8e1f43f5cf557e54c05be43d29
Christopher Choy <cchoy@cs.uchicago.edu>
2018-10-23 19:40:27 -0500

    updated publish

--- publish.sh
+++ publish.sh
@@ -1,10 +1,9 @@
+#!/bin/sh
+
 # publish.sh
 # Generates site using Hugo compiler then pushes it to gh-pages branch
 # The website pulls from our gh-pages branch to build itself.
-
 # Christopher Choy 2017
-
-#!/bin/sh
 
 DIR=~/chrischoy.github.io/
 
@@ -16,28 +15,29 @@ then
     exit 1;
 fi
 
-echo "Pulling gh-pages."
+# echo "Pulling gh-pages."
 git pull
 
-echo "Deleting old publication"
+# echo "Deleting old publication"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
+# echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Cleaning out public/"
+# echo "Cleaning out public/"
 rm -rf public/*
 
-echo "Generating site"
+# echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+# echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-echo "Pushing to github"
+# echo "Pushing to github"
 git push origin gh-pages
 
 echo "Publish Complete. Update the Custom Domain field in the Repo Settings."
+echo "https://github.com/cchoy96/chrischoy.github.io/settings"
