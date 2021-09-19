860f4ca7079fca529fddcdcf5f8ff720888acc6b
Christopher Choy <cchoy@cs.uchicago.edu>
2021-09-19 14:31:56 -0400

    update readability

--- publish.sh
+++ publish.sh
@@ -9,14 +9,14 @@ cd "$(dirname "$0")"
 
 if [[ $(git status -s) ]]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
+    echo "The working directory is dirty. \033[0;31mPlease commit any pending changes.\033[0m"
     exit 1;
 fi
 
 # echo "Pulling gh-pages."
 git pull
 
-# echo "Deleting old publication"
+echo "\033[0;34mDeleting old publication.\033[0m"
 rm -rf public
 mkdir public
 git worktree prune
@@ -28,14 +28,14 @@ git worktree add -B gh-pages public origin/gh-pages
 # echo "Cleaning out public/"
 rm -rf public/*
 
-# echo "Generating site"
+echo "\033[0;34mGenerating site.\033[0m"
 hugo --gc --minify
 
 # echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-# echo "Pushing to github"
+echo "\033[0;34mPushing to github.\033[0m"
 git push origin gh-pages
 
-echo "\n\nPublish Complete. Update the Custom Domain field in the Repo Settings:"
-echo "https://github.com/cchoy96/chrischoynet/settings\n"
+echo "\n\nPublish Complete. \033[0;31mUpdate the Custom Domain field in the Repo Settings:\033[0m"
+echo "https://github.com/cchoy96/chrischoynet/settings/pages\n"
